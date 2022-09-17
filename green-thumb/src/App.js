289d2fa806bd1524e5b8e0885d5aa8a8c2dd9465
ManasiPatil2117 <manasipatilap2117@gmail.com>
2022-09-17 04:06:44 +0000

    Background and Navbar

--- green-thumb/src/App.js
+++ green-thumb/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
-
+import bg2 from "./images/bg2.jpg";
+import Navbar from "./Components/Navbar";
 function App() {
+  const w="100%";
+  const h="745px";
   return (
     <div className="App">
-     
+     <img className="img-responsive" src={bg2} alt="Could not load" width={w} height={h}/>
+    <Navbar/>
     </div>
   );
 }
