c5ed725632d3b4d8ded3cc6fdefabc0bc6fa9462
ManasiPatil2117 <manasipatilap2117@gmail.com>
2022-09-17 11:28:15 +0000

    Routing

--- green-thumb/src/App.js
+++ green-thumb/src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import "./Animation.css";
+import About from "./Components/About";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- green-thumb/src/Components/Navbar.js
+++ green-thumb/src/Components/Navbar.js
@@ -1,29 +1,32 @@
+import {Link} from 'react-router-dom'
 import React from 'react'
 export default function Navbar() {
-    const link="/";
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-dark" >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Green Thumb</a>
+                    <Link to="/" className="navbar-brand">Green Thumb</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href={link}>Home</a>
+                                <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href={link}>Collections</a>
-                            </li>
-                           
-                            <li className="nav-item">
-                                <a className="nav-link" href={link}>About us</a>
+                                <Link to="/collections" className="nav-link">Collections</Link>
+
                             </li>
                            
                             <li className="nav-item">
-                                <a className="nav-link" href={link}>Contact us</a>
+                                <Link to="/about" className="nav-link">About us</Link>
+                            </li>
+                           
+                            <li className="nav-item">
+                                <Link to="/contact" className="nav-link">Contact us</Link>
+                                
                             </li>
                            
                         </ul>
