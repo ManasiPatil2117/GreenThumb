b4506f26c425af9d14742c14b7d976d8d01657a3
Pranav325 <bandiwadekar170@gmail.com>
2022-09-19 16:02:53 +0000

    added data/plantList

---
+++ green-thumb/src/Data/plantList.js
@@ -0,0 +1,52 @@
+import plant1 from '../images/plant1.jpg'
+import plant2 from '../images/plant2.jpg'
+import plant3 from '../images/plant3.jpg'
+import plant4 from '../images/plant4.jpg'
+import plant5 from '../images/plant5.jpg'
+import plant6 from '../images/plant6.jpg'
+
+export const plantList = [
+  {
+    name: 'Swiss Cheese Plant',
+    id: '1ed',
+    cover: plant1,
+    desc: 'Swiss Cheese palnt: The Swiss cheese plant (Monstera adansonii) gets its common name from its large, heart-shaped leaves that develop holes as the plant ages (in a process called fenestration). This makes the leaves resemble Swiss cheese. Native to Central and South America, the Swiss cheese plant is a tropical perennial that is typically grown as an indoor plant.'
+  },
+  {
+    name: 'Howea Belmoreana',
+    id: '2ab',
+    cover: plant2,
+    desc: 'Howea Belmoreana:The scientific name is Howea Belmoreana and it is also known as the curly palm or sentry palm.The sentry is native to Lord Howe Island (Australia) and cultivated there. The seeds are also sent to and cultivated in other countries including, the Hawaiian Islands.Because the fronds grow wide and the tree can grow well over 8ft tall - a suitable displaying space is required, such as a hallway or hotel entrance.'
+
+  },
+
+  {
+    name: 'Ceratopteris Thalictrodes',
+    id: '3sd',
+    cover: plant3,
+    desc: 'Ceratopteris thalictroides is a pantropical fern, 15-30 cm high and 10-20 cm wide. This plant normally grows fast, but the addition of CO2 may be necessary to promote growth. In small, open aquariums, it can grow out of the tank and form beautiful surface leaves. The finely branched leaves are very decorative and provide good contrast to other leaf shapes.'
+
+  },
+  {
+    name: 'Sisal Plant',
+    id: '4kk',
+    cover: plant4,
+    desc : 'Sisal is a species of flowering plant native to southern Mexico, but widely cultivated and naturalized in many other countries. It yields a stiff fibre used in making rope and various other products. The sisal fibre is traditionally used for rope and twine, and has many other uses, including paper, cloth, footwear, hats, bags, carpets, geotextiles, and dartboards.'
+    
+  },
+  {
+    name: 'Cape Jasmine',
+    id: '5pl',
+    cover: plant5,
+    desc: 'Cape Jasmine, also known as Gardenia augusta, is a fragrant flowering evergreen tropical plant, a favorite in gardens worldwide. It originated in Asia and is most commonly found growing in Vietnam, Southern China, Taiwan, Japan and India. As a tropical plant,it thrives best in warm temperatures in humid environments. It prefers bright indirect sunlight or partial shade, rather than direct sunlight.'
+    
+  },
+
+  {
+    name: 'Laceleaf Plant',
+    id: '8fp',
+    cover: plant6,
+    desc: 'Laceleaf plant is a genus of over 1,000 species consisting flowering plants and herbs that grow as epiphytes or terrestrial plants. The leaves of this species vary in shape and grow in a cluster form. The flowers are small in size and often perfect, which mean they carry both female and male structure to reproduce. The flowers grow in cluster on the spadix and form a spiral shape. The plant requires moist soil all year round, which should be peat moss based and well-drained as well.'
+    
+  }
+]
