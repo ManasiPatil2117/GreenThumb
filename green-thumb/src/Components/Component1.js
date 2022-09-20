933e2585f4c998e09417949c8e73dd0ecae6fe50
PrasanthMarimuthuk <prasanthchitra08@gmail.com>
2022-09-20 13:13:38 +0000

    modified components1

--- green-thumb/src/Components/Component1.js
+++ green-thumb/src/Components/Component1.js
@@ -2,6 +2,8 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import ReactRoundedImage from "react-rounded-image";
 import Button from 'react-bootstrap/Button';
 import pic1 from '../images/plant1.jpg'
 import pic2 from '../images/plant2.jpg';
@@ -10,42 +12,85 @@ import pic4 from '../images/plant4.jpg';
 import pic5 from '../images/plant5.jpg';
 import pic6 from '../images/plant6.jpg';
 import '../Styles/index.css';
-const myStyle= {
-  width :'300px', height :'300px',
-};
 
 function Component1() {
   return (
     <Container className='hai'>
       <Row>
-        <Col sm>
-        <img style={myStyle} src={pic1} className='img-fluid' alt='plant img'/>  
-      <Row><Button variant="outline-success"><h4>Swiss Cheese Plant</h4></Button></Row>
+        <Col>
+        <ReactRoundedImage
+          image={pic1}
+          roundedColor=" #00FF00"
+          imageWidth="150"
+          imageHeight="150"
+          roundedSize="20"
+          borderRadius="180"
+          align="center"
+        />
+      <Card style={{ width: '10rem' }}><Button variant="outline-success" size='sm'><Card.Title>Swiss Cheese Plant</Card.Title></Button></Card>
         </Col>
-        <Col sm>
-        <img  style={myStyle} src={pic2} className='img-fluid' alt='plant2'/>
-        <Row><Button variant="outline-success"><h4>Howea Belmoreana</h4></Button></Row>
+        <Col>
+        <ReactRoundedImage
+          image={pic2}
+          roundedColor="#00FF00"
+          imageWidth="150"
+          imageHeight="150"
+          roundedSize="20"
+          borderRadius="180"
+        />
+        <Card style={{ width: '10rem' }}><Button variant="outline-success"><Card.Title>Howea Belmoreana</Card.Title></Button></Card>
         </Col>
-        <Col sm>
-        <img style={myStyle} src={pic3} className='img-fluid' alt='hello'/>
-        <Row><Button variant="outline-success"><h4>Ceratopteris{'\n'}Thalictroides</h4></Button></Row>
+        <Col>
+        <ReactRoundedImage
+            image={pic3}
+            roundedColor="#00FF00"
+          imageWidth="150"
+          imageHeight="150"
+          roundedSize="20"
+          borderRadius="180"
+          />
+        <Card style={{ width: '10rem' }}><Button variant="outline-success"><Card.Title>Ceratopteris Thalictroides</Card.Title></Button></Card>
         </Col>
       </Row>
       <Row>
-        <Col  xs={4}>
-        <img style={myStyle}  src={pic4} className='img-fluid' alt='plant img'/>
-        <Row><Button variant="outline-success"><h4>Sisal Plant</h4></Button>{''}</Row>
+        <Col>
+        <ReactRoundedImage
+          image={pic4}
+          roundedColor="#00FF00"
+          imageWidth="150"
+          imageHeight="150"
+          roundedSize="20"
+          borderRadius="180"
+        />
+        
+        <Card style={{ width: '10rem' }}><Button variant="outline-success"><Card.Title>Sisal Plant</Card.Title></Button></Card>
         </Col>
-        <Col xs={4} >
-        <img style={myStyle} src={pic5} className='img-fluid' alt='plant2'/>
-        <Row><Button variant="outline-success"><h4>Cape jasmine</h4></Button></Row>
+        <Col>
+        <ReactRoundedImage
+          image={pic5}
+          roundedColor="#00FF00"
+          imageWidth="150"
+          imageHeight="150"
+          roundedSize="20"
+          borderRadius="180"
+        />
+        <Card style={{ width: '10rem' }}><Button variant="outline-success"><Card.Title>Cape jasmine</Card.Title></Button></Card>
         </Col>
-        <Col xs={4} >
-        <img style={myStyle} src={pic6} className='img-fluid' alt='hello'/>
-          <Row><Button variant="outline-success"><h4>Laceleaf plant</h4></Button></Row>
+        <Col>
+        <ReactRoundedImage
+          image={pic6}
+          roundedColor="#00FF00"
+          imageWidth="150"
+          imageHeight="150"
+          roundedSize="20"
+          borderRadius="180"
+        />
+          <Card style={{ width: '10rem' }}><Button variant="outline-success"><Card.Title>Laceleaf plant</Card.Title></Button></Card>
         </Col>
       </Row>
       
+
+        
     </Container>
   );
 }
