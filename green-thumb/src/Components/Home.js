06b47dc2e861127783f28b48310f09f926c41ef0
ManasiPatil2117 <manasipatilap2117@gmail.com>
2022-09-21 14:15:07 +0000

    Explore btn

--- green-thumb/src/Components/Home.js
+++ green-thumb/src/Components/Home.js
@@ -1,23 +1,26 @@
 import React from "react";
-import {Route, Routes, useNavigate} from 'react-router-dom'
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Collections from "./Collections";
+import "../Styles/Animation.css";
 
 export default function Home() {
   const navigate = useNavigate();
-  const navigateCollections= ()=>{
-    navigate('/collections');
+  const navigateCollections = () => {
+    navigate("/collections");
   };
   return (
     <>
       <div className="container mx-auto mt-5 " align="center">
         <span className="Animation">Green Thumb</span>
-        <div className="fs-3 fw-bolder">
+        <div className="fs-3 fw-bolder text-white">
           Where you can buy any plant and we will deliver it straight to your
           door
         </div>
-        <button className="btn mt-5" onClick={navigateCollections}>Explore</button>
+        <button className="btn1 mt-5" onClick={navigateCollections}>
+          Explore
+        </button>
         <Routes>
-            <Route path="/collections" element={<Collections/>}/>
+          <Route path="/collections" element={<Collections />} />
         </Routes>
       </div>
     </>
