c5f0029ecf45b4c1ae6ef652970b6323070f1013
Pranav325 <bandiwadekar170@gmail.com>
2022-09-25 10:37:54 +0000

    added desc modal

--- green-thumb/src/Components/Collections.js
+++ green-thumb/src/Components/Collections.js
@@ -3,18 +3,29 @@ import PlantItem from './PlantItem'
 import '../Styles/Collections.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import React,{useState} from 'react';
+import Modal from './Modal'
 
 function Collections() {
+const [model,setModel] = useState(false);
+const [tempdata, setTempdata] = useState([]);
+
+const viewDesc = (name,cover,desc) =>{
+  let tempData = [name,cover,desc];
+  setTempdata([1, ...tempData])
+  return setModel(true);
+
+  }
   return (
     <div className='collection-list'>
 
       <ul className='plant-list'>
-        {plantList.map(({ id, cover, name}) =>
+        {plantList.map(({ id, cover, name, desc}) =>
           (
             <div key={id}>
               <PlantItem
                 cover={cover}
-                name={<Card.Title>.<Card style={{ width: '15rem' }}><Button variant="outline-success" size='sm'><Card.Title>{name}</Card.Title></Button></Card>.</Card.Title>}
+                name={<Card.Title>.<Card style={{ width: '15rem' }}><Button variant="outline-success" size='sm' onClick={()=>viewDesc(name,cover,desc)}><Card.Title>{name}</Card.Title></Button></Card>.</Card.Title>}
                 
               />
               
@@ -22,6 +33,12 @@ function Collections() {
           ) 
         )}
       </ul>
+
+      {
+        model === true ? <Modal name={tempdata[1]} cover={tempdata[2]} desc={tempdata[3]} hide={()=>setModel(false)}/>: ''
+      }
+
+      
     </div>
   )
 }
---
+++ green-thumb/src/Components/Modal.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react'
+
+export default class Modal extends Component{
+    render(){
+        let modelStyle ={
+            display: 'block',
+            backgroundColor: 'rgba(0,0,0,0.8)',
+        }
+        return(
+            <div className="modal show fade" style={modelStyle}>
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title">{this.props.name}</h5>
+        <button type="button" className="btn-close" onClick={this.props.hide}></button>
+      </div>
+      <div className="modal-body">
+        <img src={this.props.cover} className="img-fluid" alt=''/>
+        <p>{this.props.desc}</p>
+      </div>
+      
+    </div>
+  </div>
+</div>
+        )
+    }
+}
