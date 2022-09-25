c989eaa59f7eb6fcab450f4370995106d081f46a
Pranav325 <bandiwadekar170@gmail.com>
2022-09-25 07:00:40 +0000

    added new plants

--- green-thumb/src/Components/Collections.js
+++ green-thumb/src/Components/Collections.js
@@ -1,6 +1,8 @@
 import { plantList } from '../Data/plantList'
 import PlantItem from './PlantItem'
 import '../Styles/Collections.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Collections() {
   return (
@@ -12,7 +14,8 @@ function Collections() {
             <div key={id}>
               <PlantItem
                 cover={cover}
-                name={name}
+                name={<Card.Title>.<Card style={{ width: '15rem' }}><Button variant="outline-success" size='sm'><Card.Title>{name}</Card.Title></Button></Card>.</Card.Title>}
+                
               />
               
             </div>
